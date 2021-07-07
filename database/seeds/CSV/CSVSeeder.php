5ea59c35788b9d5b4a0f0fb82902372b80fda386
N1ebieski <mariusz.wysokinski@neostrada.pl>
2021-07-07 11:07:43 +0200

    refactor

--- database/seeds/CSV/CSVSeeder.php
+++ database/seeds/CSV/CSVSeeder.php
@@ -3,11 +3,7 @@
 namespace App\Seeds\CSV;
 
 use Illuminate\Database\Seeder;
-use N1ebieski\IDir\Models\User;
-use N1ebieski\IDir\Models\Group;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Contracts\Queue\Queue;
-use N1ebieski\IDir\Models\Field\Field;
 use N1ebieski\IDir\Seeds\Traits\Importable;
 use Illuminate\Contracts\Cache\Factory as Cache;
 
@@ -23,109 +19,16 @@ class CSVSeeder extends Seeder
     protected $cache;
 
     /**
-     * Undocumented variable
-     *
-     * @var Queue
-     */
-    protected $queue;
-
-    /**
-     * Undocumented variable
-     *
-     * @var int
-     */
-    public $groupLastId;
-
-    /**
-     * Undocumented variable
-     *
-     * @var int
-     */
-    public $fieldLastId;
-
-    /**
-     * Undocumented variable
-     *
-     * @var int
-     */
-    public $subLastId;
-
-    /**
-     * Undocumented variable
-     *
-     * @var int
-     */
-    public $userLastId;
-
-    /**
      * Undocumented function
      *
      * @param Cache $cache
      */
-    public function __construct(Cache $cache, Queue $queue)
+    public function __construct(Cache $cache)
     {
         $this->cache = $cache;
-        $this->queue = $queue;
-
-        // $this->groupLastId = static::groupLastId();
-        // $this->fieldLastId = static::fieldLastId();
-        // $this->subLastId = static::subLastId();
-        // $this->userLastId = static::userLastId();
 
         DB::disableQueryLog();
     }
-
-    // /**
-    //  * Undocumented function
-    //  *
-    //  * @return integer
-    //  */
-    // protected static function userLastId() : int
-    // {
-    //     return (
-    //         User::orderBy('id', 'desc')->first()->id
-    //         -
-    //         DB::connection('import')->table('users')->orderBy('id', 'desc')->first()->id
-    //     );
-    // }
-
-    // /**
-    //  * Undocumented function
-    //  *
-    //  * @return integer
-    //  */
-    // protected static function subLastId() : int
-    // {
-    //     return DB::connection('import')->table('subcategories')->orderBy('id', 'desc')->first('id')->id;
-    // }
-
-    // /**
-    //  * Undocumented function
-    //  *
-    //  * @return integer
-    //  */
-    // protected static function fieldLastId() : int
-    // {
-    //     return (
-    //         (Field::orderBy('id', 'desc')->first()->id ?? 0)
-    //         -
-    //         optional(DB::connection('import')->table('forms')->where('mod', 0)->orderBy('id', 'desc')->first())->id
-    //     );
-    // }
-
-    // /**
-    //  * Undocumented function
-    //  *
-    //  * @return integer
-    //  */
-    // protected static function groupLastId() : int
-    // {
-    //     return (
-    //         Group::orderBy('id', 'desc')->first()->id
-    //         -
-    //         DB::connection('import')->table('groups')->orderBy('id', 'desc')->first()->id
-    //     );
-    // }
 
     /**
      * Seed the application's database.
