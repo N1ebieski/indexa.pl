e18853355ea8123d158125630514d4cd00463534
N1ebieski <mariusz.wysokinski@neostrada.pl>
2021-07-07 12:46:17 +0200

    fix

--- database/seeds/CSV/CSVSeeder.php
+++ database/seeds/CSV/CSVSeeder.php
@@ -3,7 +3,11 @@
 namespace App\Seeds\CSV;
 
 use Illuminate\Database\Seeder;
+use N1ebieski\IDir\Models\User;
+use N1ebieski\IDir\Models\Group;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Contracts\Queue\Queue;
+use N1ebieski\IDir\Models\Field\Field;
 use N1ebieski\IDir\Seeds\Traits\Importable;
 use Illuminate\Contracts\Cache\Factory as Cache;
 
@@ -19,13 +23,21 @@ class CSVSeeder extends Seeder
     protected $cache;
 
     /**
+     * Undocumented variable
+     *
+     * @var Queue
+     */
+    protected $queue;
+
+    /**
      * Undocumented function
      *
      * @param Cache $cache
      */
-    public function __construct(Cache $cache)
+    public function __construct(Cache $cache, Queue $queue)
     {
         $this->cache = $cache;
+        $this->queue = $queue;
 
         DB::disableQueryLog();
     }
