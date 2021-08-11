8718322577caca14674f22e3283e24b32fae30b4
N1ebieski <srv38307@h23.seohost.pl>
2021-08-11 14:18:38 +0200

    client changes

--- storage/framework/views/8ba616f02cce54dfbc6deedf26b078085dd042d3.php
+++
@@ -1,6 +0,0 @@
-<?php if($links->isNotEmpty()): ?>
-    <?php $__currentLoopData = $links; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $link): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-        <?php echo $link->link_as_html; ?> |
-    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-<?php endif; ?>
-<?php /**PATH /home/srv38307/domains/indexa.pl/resources/views/vendor/icore/web/components/link.blade.php ENDPATH**/ ?>
